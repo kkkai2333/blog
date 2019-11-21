60dd671ed1de57be3c464af512fb0d693b251c9f
Zhang, Damon X. -ND <Damon.X.Zhang.-ND@disney.com>
2019-11-21 14:03:49 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -19,10 +19,8 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:Damon-kz/Damon-kz.github.io.git master
-git push -f https://81c5540a281599807c3c4bb54279bd7476c3a49c@github.com/Damon-kz/Damon-kz.github.io.git master
-# git push -f https://${GITHUB_TOKEN}@github.com/Damon-kz/Damon-kz.github.io.git master
-# 3b8822bcadb3ac1e10e70ae20d9847c963a4ec9b
-# 81c5540a281599807c3c4bb54279bd7476c3a49c
+# git push -f https://81c5540a281599807c3c4bb54279bd7476c3a49c@github.com/Damon-kz/Damon-kz.github.io.git master:gh-pages
+git push -f https://${GITHUB_TOKEN1}@github.com/Damon-kz/Damon-kz.github.io.git master:gh-pages
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
--- docs/README.md
+++ docs/README.md
@@ -1,16 +1,16 @@
 ---
 home: true
-heroImage: /logo.png
+heroImage: ../logo.png
 heroText: Code Learn
 tagline: stay hungry stay foolish
 actionText: 马上开始
 actionLink: /java/
 features:
 - title: Java知识
-  details: 
+  details: 每天都有新知识
 - title: 算法题目
-  details:
+  details: 来一道算法题更开胃
 - title: 其他分享
-  details: 
+  details: 分享一些读书笔记
 footer: MIT Licensed | Copyright © 2019-present Damon
 ---
