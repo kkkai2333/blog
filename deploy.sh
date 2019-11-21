89aea610dad8cebe5405c9c07b8adb5053435805
jikzhang <zhangjikai5@gmail.com>
2019-11-21 22:13:32 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # 如果要发布到私人域名
-echo 'holmess.top' > CNAME
+echo 'www.holmess.top' > CNAME
 
 # 提交到github
 # git init
@@ -22,7 +22,7 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:Damon-kz/Damon-kz.github.io.git master
 # git push -f https://81c5540a281599807c3c4bb54279bd7476c3a49c@github.com/Damon-kz/Damon-kz.github.io.git master:gh-pages
-git push -f https://${GITHUB_TOKEN1}@github.com/Damon-kz/Damon-kz.github.io.git master:gh-pages
+git push -f https://${GITHUB_TOKEN1}@github.com/Damon-kz/blogsite.git master:gh-pages
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
