6410d145bcb68179e00d811224d997e72de5c669
Zhang, Damon X. -ND <Damon.X.Zhang.-ND@disney.com>
2019-11-25 14:09:53 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -15,14 +15,13 @@ cd docs/.vuepress/dist
 echo 'blog.holmess.top' > CNAME
 
 # 提交到github
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:Damon-kz/Damon-kz.github.io.git master
 # git push -f https://81c5540a281599807c3c4bb54279bd7476c3a49c@github.com/Damon-kz/Damon-kz.github.io.git master:gh-pages 
-git push -f https://${GITHUB_TOKEN1}@github.com/Damon-kz/Damon-kz.github.io.git master # 3db5d6178ee7fbdac0bbb0da78a938d16380bef8
+# git push -f https://${GITHUB_TOKEN1}@github.com/Damon-kz/Damon-kz.github.io.git master # 3db5d6178ee7fbdac0bbb0da78a938d16380bef8
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
