224f1d6aa052d46c131c9f5717d56a4f47b08b69
jikaizhang <jikaizhang@trip.com>
2021-07-23 10:07:29 +0800

    deploy

--- git_push.sh
+++ git_push.sh
@@ -3,6 +3,4 @@ echo '=====git push code start====='
 git add -A
 git commit -m 'deploy'
 git push
-git push four master
-
 echo '=====git push code end====='
--- git_push_all.sh
+++ git_push_all.sh
@@ -3,5 +3,6 @@ echo '=====git push code start====='
 git add -A
 git commit -m 'deploy'
 git push
+git push four master
 
 echo '=====git push code end====='
